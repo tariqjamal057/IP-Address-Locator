eaf79b5ebf301518626ae92a18859ac2a30a102c
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-08-10 14:09:36 +0530

    refactor: add jinja template linter

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 def home():
     template = render_template(template_name_or_list="index.html")
     if request.method == "POST":
-        ip_address = request.form.get("ip_data")
-        response = requests.get(settings.IP_ADDRESS_API.replace("IP_ADDRESS_API_KEY", settings.IP_ADDRESS_API_KEY).replace("IP_ADDRESS", ip_address))
-        data = response.json()
-        status = True if response.status_code == 200 else False
-        template = render_template(template_name_or_list="index.html", data=data, ip_address = ip_address, status = status)
+        ip_address = request.form.get("ip_data", None)
+        if ip_address:
+            response = requests.get(settings.IP_ADDRESS_API.replace("IP_ADDRESS_API_KEY", settings.IP_ADDRESS_API_KEY).replace("IP_ADDRESS", ip_address))
+            data = response.json()
+            template = render_template(template_name_or_list="index.html", data=data, ip_address = ip_address, status = response.status_code)
 
     return template
