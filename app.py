07e0c8eb2e3131e6f68fd6b0045f1060971d9c77
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-12-08 01:38:05 +0530

    refactor: code format

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 import requests
 from flask import Flask, render_template, request
-from settings import settings
-import requests
-from flask import Flask, render_template, request
+
 from settings import settings
 
 app = Flask(__name__)
 
 
-@app.route("/",methods = ['GET', 'POST'])
-@app.route("/",methods = ['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
     template = render_template(template_name_or_list="index.html")
     if request.method == "POST":
         ip_address = request.form.get("ip_data", None)
         if ip_address:
-            response = requests.get(settings.IP_ADDRESS_API.replace("IP_ADDRESS_API_KEY", settings.IP_ADDRESS_API_KEY).replace("IP_ADDRESS", ip_address))
+            response = requests.get(
+                settings.IP_ADDRESS_API.replace(
+                    "IP_ADDRESS_API_KEY", settings.IP_ADDRESS_API_KEY
+                ).replace("IP_ADDRESS", ip_address)
+            )
             data = response.json()
-            template = render_template(template_name_or_list="index.html", data=data, ip_address = ip_address, status = response.status_code)
+            template = render_template(
+                template_name_or_list="index.html",
+                data=data,
+                ip_address=ip_address,
+                status=response.status_code,
+            )
 
     return template
--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     IP_ADDRESS_API_KEY: str
     IP_ADDRESS_API: str
+
 
 settings = Settings()
